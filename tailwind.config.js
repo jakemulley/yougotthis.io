2bdc4fd8a1af46cfc9dbc80bf5096868546ea8ae
Kevin Lewis <kevin.lewis@vonage.com>
2020-08-13 14:41:58 +0100

    incl typography in purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,11 @@ module.exports = {
   plugins: [
     require('@tailwindcss/typography')
   ],
-  purge: [
-    './build/*.html',
-    './build/**/*.html'
-  ],
+  purge: {
+    mode: 'all',
+    content: [
+      './build/*.html',
+      './build/**/*.html'
+    ]
+  },
 }
