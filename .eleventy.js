6cae1e648ae6f70c40e3b09c3e9efb28d669b12b
Kevin Lewis <kevin.lewis@vonage.com>
2020-08-30 17:51:44 +0100

    Fixed RSS feed

--- .eleventy.js
+++ .eleventy.js
@@ -1,24 +1,26 @@
-const htmlmin = require("html-minifier");
+const htmlmin = require('html-minifier')
 
-module.exports = function(config) {
+module.exports = function (config) {
+  config.setQuietMode(true)
 
   config.addPassthroughCopy('src/assets/img')
   config.addPassthroughCopy('src/assets/js')
   config.addPassthroughCopy('src/assets/couch')
   config.addPassthroughCopy('src/_redirects')
 
-  config.addCollection("talks", collection => {
+  config.addCollection('talks', collection => {
     return collection.getFilteredByTag('talk').sort((a, b) => {
-      const A = a.data.talk.title;
-      const B = b.data.talk.title;
-      if(A > B) return 1
-      else if (A < B) return -1;
-      else return 0;
-    });
+      const A = a.data.talk.title
+      const B = b.data.talk.title
+      if (A > B) return 1
+      else if (A < B) return -1
+      else return 0
+    })
   })
 
-  config.addCollection("rss", collection => {
-    return collection.getFilteredByTag('talk')
+  config.addCollection('rss', collection => {
+    return collection
+      .getFilteredByTag('talk')
       .map(t => {
         return {
           data: {
@@ -28,20 +30,20 @@ module.exports = function(config) {
               abstract: t.data.talk.abstract.split('<br />').join(' ')
             },
             pubDate: t.data.date.toUTCString()
-          },
+          }
         }
       })
       .sort((a, b) => {
-        const A = a.data.date;
-        const B = b.data.date;
-        if(A < B) return 1
-        else if (A > B) return -1;
-        else return 0;
-      });
+        const A = a.data.date
+        const B = b.data.date
+        if (A < B) return 1
+        else if (A > B) return -1
+        else return 0
+      })
   })
 
-  config.addTransform("htmlmin", (content, outputPath) => {
-    if(outputPath.endsWith(".html")) {
+  config.addTransform('htmlmin', (content, outputPath) => {
+    if (outputPath.endsWith('.html')) {
       return htmlmin.minify(content, {
         useShortDoctype: true,
         removeComments: true,
