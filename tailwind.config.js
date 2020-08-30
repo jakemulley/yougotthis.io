bf8c9881cc950185f39baa134f41b80a9779b48a
Kevin Lewis <kevin.lewis@vonage.com>
2020-08-31 00:10:55 +0100

    dark-mode header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
-    plugins: [require('@tailwindcss/typography')],
-    purge: {
-        mode: 'all',
-        content: ['./build/*.html', './build/**/*.html']
+  plugins: [require('@tailwindcss/typography')],
+  purge: {
+    mode: 'all',
+    content: ['./build/*.html', './build/**/*.html']
+  },
+  theme: {
+    extend: {
+      screens: {
+        dark: { raw: '(prefers-color-scheme: dark)' }
+      }
     }
+  }
 }
